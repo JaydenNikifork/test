fbcaf50f82950650711ea2e4a11c1b4c7dc79bf9
Jayden Nikifork <90576596+JaydenNikifork@users.noreply.github.com>
2024-04-17 17:45:21 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,28 +1,13 @@
-import { marked } from "marked";
-import { Test } from "./test1/test2/test3/index"
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    Promise.resolve().then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
 
-export const entry = () => {
-    Test();
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Simple Web Page</title>
-    </head>
-    <body>
-        <header>
-            <h1>Welcome to My Website!</h1>
-        </header>
-        <main>
-            <p>This is a simple HTML web page. You can customize it with your own content, style it with CSS, and make it interactive with JavaScript. Enjoy creating!</p>
-            <a href="https://www.example.com">Learn More</a>
-        </main>
-        <footer>
-            <p>© 2024 Web Page Creator. All rights reserved.</p>
-        </footer>
-    </body>
-    </html>
-    `
-}
+export default reportWebVitals;
